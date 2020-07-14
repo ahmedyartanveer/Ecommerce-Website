b2fe1a0ff12001ea7e2d291505124fca2038c04f
ahmedyar123 <ahmeda.abbasi99@gmail.com>
2020-07-15 03:33:55 +0500

    Added setProducts function in context.js

--- src/context.js
+++ src/context.js
@@ -11,6 +11,21 @@ class ProductProvider extends Component {
     detailProduct: detailProduct,
   };
 
+  componentDidMount() {
+    this.setProducts();
+  }
+
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach((item) => {
+      const singleItem = { ...item };
+      tempProducts = { ...tempProducts, singleItem };
+    });
+    this.setState(() => {
+      return { products: tempProducts };
+    });
+  };
+
   handleDetail = () => {
     console.log("hello from handle detail");
   };
@@ -18,6 +33,23 @@ class ProductProvider extends Component {
   handleAddToCart = () => {
     console.log("hello from add to cart");
   };
+
+  //   tester = () => {
+  //     console.log("State Products", this.state.products[0].inCart);
+  //     console.log("Data Products", storeProducts[0].inCart);
+
+  //     const tempProducts = { ...this.state.products };
+  //     tempProducts[0].inCart = true;
+  //     this.setState(
+  //       () => {
+  //         return { products: tempProducts };
+  //       },
+  //       () => {
+  //         console.log("State Products", this.state.products[0].inCart);
+  //         console.log("Data Products", storeProducts[0].inCart);
+  //       }
+  //     );
+  //   };
 
   render() {
     return (
@@ -28,6 +60,7 @@ class ProductProvider extends Component {
           handleAddToCart: this.handleAddToCart,
         }}
       >
+        {/* <button onClick={this.tester}>test me</button> */}
         {this.props.children}
       </ProductContext.Provider>
     );
