804810355d19c0ffecd84f6046555cd6fa86118b
ahmedyar123 <ahmeda.abbasi99@gmail.com>
2020-07-21 02:49:16 +0500

    Updated some boostrap classes in productList.js

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -4,6 +4,7 @@ import Title from "./Title";
 import { storeProducts } from "../data";
 import styled from "styled-components";
 import { ProductConsumer } from "../context";
+
 export default class ProductList extends Component {
   state = {
     products: storeProducts,
@@ -12,9 +13,9 @@ export default class ProductList extends Component {
     return (
       <React.Fragment>
         <ProductWrapper className="py-5">
-          <div className="container justify-content-center">
+          <div className="container">
             <Title name="our" title="products" />
-            <div className="row d-flex justify-content-center">
+            <div className="row">
               <ProductConsumer>
                 {(value) => {
                   return value.products.map((product) => {
