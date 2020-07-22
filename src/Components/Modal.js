a1e7ac33229438e8ee0a92a9f5660b07b6573038
ahmedyar123 <ahmeda.abbasi99@gmail.com>
2020-07-23 02:49:23 +0500

    Removed br and added bootstrap rounded class

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -21,7 +21,7 @@ export default class Modal extends Component {
                   <div className="row">
                     <div
                       id="modal"
-                      className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize"
+                      className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize rounded"
                     >
                       <h5>Item added to the cart</h5>
                       <img src={img} className="img-fluid" alt="product" />
@@ -32,7 +32,6 @@ export default class Modal extends Component {
                           Store
                         </ButtonContainer>
                       </Link>
-                      <br />
                       <Link to="/cart">
                         <ButtonContainer cart onClick={() => closeModal()}>
                           go to cart
